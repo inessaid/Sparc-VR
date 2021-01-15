7e81491e37fbacde1d9a94e5e1d5a8a39c5ce1f2
Bingyu Li <bingyuli0428@gmail.com>
2021-01-15 13:47:09 -0500

    IK Animation

---
+++ Assets/HandRotateControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandRotateControl : MonoBehaviour
+{
+
+    public GameObject rHand;
+    public Vector3 offset;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        transform.position = new Vector3 (rHand.transform.position.x, rHand.transform.position.y, rHand.transform.position.z) + offset;
+
+        transform.rotation = new Quaternion(rHand.transform.rotation.x, rHand.transform.rotation.y, rHand.transform.rotation.z, 1);
+    }
+}
